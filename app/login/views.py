1f4452add307e72f819b043e04ad95471355b29f
Krittanut Jirathawong <j.krittanut@gmail.com>
2020-01-31 00:07:43 +0700

    basic database retrieval

--- app/login/views.py
+++ app/login/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import User
+
 def index(request):
-    return HttpResponse("You're at the login index.")
+    user_list = User.objects.order_by('name')[:5]
+    context = {'user_list': user_list}
+    return render(request, 'login/index.html', context)
