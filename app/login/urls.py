19b8bd6d5f00fe2cb04ca15f06d1536ca9c076b5
Krittanut Jirathawong <j.krittanut@gmail.com>
2020-01-31 00:49:04 +0700

    login in progress

--- app/login/urls.py
+++ app/login/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'login'
+
 urlpatterns = [
     path('', views.index, name='index'),
+    path('resolve_login/', views.resolve_login, name='resolve_login'),
+    path('result/', views.result, name='result')
 ]
--- app/login/views.py
+++ app/login/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 from .models import User
 
@@ -7,3 +8,9 @@ def index(request):
     user_list = User.objects.order_by('name')[:5]
     context = {'user_list': user_list}
     return render(request, 'login/index.html', context)
+
+def resolve_login(request):
+    return HttpResponseRedirect(reverse('login:result'))
+
+def result(request):
+    return HttpResponse("result")
