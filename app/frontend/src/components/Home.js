5cf6d1a4d85422bc8c5d21c1fb37a2801f8e426e
Krittanut Jirathawong <j.krittanut@gmail.com>
2020-03-20 04:13:10 +0700

    Added homepage

--- app/frontend/src/components/Home.js
+++ app/frontend/src/components/Home.js
@@ -1,3 +1,6 @@
+// import React from 'react';
+import homeimg from '../homeimg.svg';
+
 import React, { Component } from "react";
 import { Col, Container, Row } from "reactstrap";
 import UserList from "./UserList";
@@ -28,24 +31,37 @@ class Home extends Component {
 
   render() {
     return (
-      <Container style={{ marginTop: "20px" }}>
+      <Container style={{ marginTop: "20px", display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
         <Row>
-          <Col>
+          {/* <Col>
             <UserList
               students={this.state.students}
               resetState={this.resetState}
             />
+          </Col> */}
+          <Col>
+            <img src={homeimg} alt="logo" />
+          </Col>
+
+          <Col>
+            <p style={{ textAlign: "left"}}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Phasellus magna nunc, lacinia sit amet nisi in, faucibus pharetra diam.
+              Integer sollicitudin interdum laoreet. Vestibulum pretium ipsum odio,
+              vitae congue magna pellentesque eu. Vestibulum a malesuada felis, eget consequat neque.
+              Sed id nibh vel ante ultrices porta. Sed lectus leo, hendrerit quis nisi dictum, finibus dignissim magna.
+            </p>
           </Col>
         </Row>
-        <Row>
+        {/* <Row>
           <Col>
             <NewUserModal create={true} resetState={this.resetState} />
           </Col>
-        </Row>
-        <Col>
-          <LoginForm />
-        </Col>
-      </Container>
+        </Row> */}
+    <Col>
+      {/* <LoginForm /> */}
+    </Col>
+      </Container >
     );
   }
 }
