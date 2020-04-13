e4e1b67690c2a657f9b689f75a690e7b1671c4be
Krittanut Jirathawong <j.krittanut@gmail.com>
2020-04-13 21:02:34 +0700

    Added references from official django doc on how to access model data

--- app/core/views.py
+++ app/core/views.py
@@ -143,3 +143,29 @@ def recalculate(request, item_idx):
     # set current_price in item row where item_idx to price_point[optimal_idx]
 
 # to get the updated price, simply get the item details as normal
+
+
+# References
+# from django.http import HttpResponse, HttpResponseRedirect
+# from django.shortcuts import get_object_or_404, render
+# from django.urls import reverse
+
+# from .models import Choice, Question
+# # ...
+# def vote(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     try:
+#         selected_choice = question.choice_set.get(pk=request.POST['choice'])
+#     except (KeyError, Choice.DoesNotExist):
+#         # Redisplay the question voting form.
+#         return render(request, 'polls/detail.html', {
+#             'question': question,
+#             'error_message': "You didn't select a choice.",
+#         })
+#     else:
+#         selected_choice.votes += 1
+#         selected_choice.save()
+#         # Always return an HttpResponseRedirect after successfully dealing
+#         # with POST data. This prevents data from being posted twice if a
+#         # user hits the Back button.
+#         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
