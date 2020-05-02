af11576119df7ea392ad3da4e02157e476a538c6
Krittanut Jirathawong <j.krittanut@gmail.com>
2020-05-02 20:06:34 +0700

    Added Connect to Unity V1

--- app/upgraded-frontend/src/views/UnityConnect.jsx
+++ app/upgraded-frontend/src/views/UnityConnect.jsx
@@ -15,9 +15,14 @@ const UnityConnect = () => {
       </div>
       <div className="typo-line">
         <p>
-          1) ewrw <br />
-          2) 213123 <br />
-          3) l;,psf <br />
+          1) Create a store. Here you can specify its name and description. <br />
+          2) Get to store management page of the store.<br />
+          3) Create an item. Specify its name.<br />
+          4) Click on (>) to view all price points associated with the item. (There will be none, as the item has just been created)<br />
+          5) Create possible price points for your item that the model can explore.<br />
+          6) Note down your store id and item id, they will be required in later steps.<br />
+          7) In your Unity Project, create a new object. In this example, we will name it “DP Integrator”.<br />
+          8) Add the following scripts to the object. The script’s name is irrelevant (in this example: Integrator Script)<br />
         </p>
       </div>
       <div className="content">
@@ -38,20 +43,18 @@ const UnityConnect = () => {
                   </div>
                 }
               />
-
-
-              {/* <Card
-                bigIcon={<i className="pe-7s-server text-warning" />}
-                statsText="Capacity"
-                statsValue="105GB"
-                statsIcon={<i className="fa fa-refresh" />}
-                statsIconText="Updated now"
-              /> */}
             </Col>
             <Col lg={1} sm={1} />
           </Row>
         </Grid>
       </div>
+      <div className="typo-line">
+        <p>
+          9) Everything is now ready. You can now start using the functions available in the script. Here are brief explanations of what each functions do to get you started:<br />
+        10) An example of a an item in a store that updates its price every 10 seconds can be found here:<br />
+        </p>
+      </div>
+
     </>
   );
 }
