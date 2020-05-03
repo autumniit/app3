8cb1c5633a33736af491d6dcca7cf475ee64be64
Krittanut Jirathawong <j.krittanut@gmail.com>
2020-05-03 19:37:15 +0700

    Unity Connect Text V2

--- app/upgraded-frontend/src/views/UnityConnect.jsx
+++ app/upgraded-frontend/src/views/UnityConnect.jsx
@@ -5,12 +5,12 @@ import { Card } from "components/Card/Card.jsx";
 
 const UnityConnect = () => {
 
-  var r;
-  const [text, setText] = useState("")
-  useEffect(async () => {
-    r = await fetch('/sample.txt')
-    setText(await r.text())
-  }, [])
+  // var r;
+  // const [text, setText] = useState("")
+  // useEffect(async () => {
+  //   r = await fetch('/sample.txt')
+  //   setText(await r.text())
+  // }, [])
 
 
   return (
@@ -24,13 +24,13 @@ const UnityConnect = () => {
       <div className="typo-line">
         <p>
           1) Create a store. Here you can specify its name and description. <br />
-          2) Get to store management page of the store.<br />
-          3) Create an item. Specify its name.<br />
-          4) Click on (>) to view all price points associated with the item. (There will be none, as the item has just been created)<br />
-          5) Create possible price points for your item that the model can explore.<br />
-          6) Note down your store id and item id, they will be required in later steps.<br />
+          2) In Manage, click <i className="pe-7s-plus" /> to create a new store. Set its name and description.<br />
+          3) Click <i className="pe-7s-note" /> to get to Store Manage. Create an item. Specify its name.<br />
+          4) Click on <i className="pe-7s-angle-right-circle" /> to view all price points associated with the item. (There will be none, as the item has just been created)<br />
+          5) Create possible price points for your item for the model to explore.<br />
+          6) Note down StoreID and ItemID of the item, they will be required in later steps.<br />
           7) In your Unity Project, create a new object. In this example, we will name it “DP Integrator”.<br />
-          8) Add the following scripts to the object. The script’s name is irrelevant (in this example: Integrator Script)<br />
+          8) Add the following scripts to the object. The script’s name is irrelevant (in this example: DPAppIntegratorScript.cs)<br />
         </p>
       </div>
       <div className="content">
@@ -44,7 +44,7 @@ const UnityConnect = () => {
                   <div className="typo-line">
                     <p className="code">
 
-                      {text}
+                      {/* {text} */}
 
                     </p>
                   </div>
@@ -57,8 +57,8 @@ const UnityConnect = () => {
       </div>
       <div className="typo-line">
         <p>
-          9) Everything is now ready. You can now start using the functions available in the script. Here are brief explanations of what each functions do to get you started:<br />
-        10) An example of a an item in a store that updates its price every 10 seconds can be found here:<br />
+          9) Everything is now ready. You can start using the functions available in the script. Here are brief explanations of what each functions do to get you started:<br />
+        10) An example of a an item in a store that updates its price every 10 seconds can be found here: <br />
         </p>
       </div>
 
