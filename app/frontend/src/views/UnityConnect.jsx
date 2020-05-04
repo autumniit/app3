27b784e51464da64558b9dbe578f165923e25783
Krittanut Jirathawong <j.krittanut@gmail.com>
2020-05-04 23:50:42 +0700

    Upgraded UnityConnect page

--- app/frontend/src/views/UnityConnect.jsx
+++ app/frontend/src/views/UnityConnect.jsx
@@ -1,7 +1,8 @@
 import React, { } from "react";
 import { Grid, Row, Col } from "react-bootstrap";
 
-import { CopyBlock, a11yLight } from 'react-code-blocks'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { okaidia } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 import { Card } from "components/Card/Card.jsx";
 
@@ -63,8 +64,7 @@ public class DPAppIntegrator : MonoBehaviour
     {
         StartCoroutine(SendRecalculateRequest(storeId, itemId, demand, callback));
     }
-...
-}`
+...`
 
   var step10code =
     `// EventScript.cs
@@ -144,129 +144,134 @@ public class EventScript : MonoBehaviour
           <Col md={12}>
             <Card
               content={
+
                 <div>
-                  <div className="typo-line">
-                    <h2>
-                      Connect to Unity <br />
-                      <small>Follow these simple steps to have your own dynamic pricing system integrated into your game.</small>
-                    </h2>
-                  </div>
-                  <div className="typo-line">
-                    <p>
-                      <span className="category">Step 1:</span>
+                  <Row><Col lg={10}>
+                    <div className="typo-line">
+                      <h2>
+                        Connect to Unity <br />
+                        <small>Follow these simple steps to have your own dynamic pricing system integrated into your game.</small>
+                      </h2>
+                    </div>
+                    <div className="typo-line">
+                      <p>
+                        <span className="category">Step 1:</span>
                       Create a store. Here you can specify its name and description.
                     </p>
-                  </div>
-                  <div className="typo-line">
-                    <p>
-                      <span className="category">Step 2:</span>
+                    </div>
+                    <div className="typo-line">
+                      <p>
+                        <span className="category">Step 2:</span>
                       In Manage, click <i className="pe-7s-plus" /> to create a new store. Set its name and description.
                     </p>
-                  </div>
-                  <div className="typo-line">
-                    <p>
-                      <span className="category">Step 3:</span>
+                    </div>
+                    <div className="typo-line">
+                      <p>
+                        <span className="category">Step 3:</span>
                       Click <i className="pe-7s-note" /> to get to Store Manage. Create an item. Specify its name.
                     </p>
-                  </div>
-                  <div className="typo-line">
-                    <p>
-                      <span className="category">Step 4:</span>
+                    </div>
+                    <div className="typo-line">
+                      <p>
+                        <span className="category">Step 4:</span>
                       Click on <i className="pe-7s-angle-right-circle" /> to view all price points associated with the item. (There will be none, as the item has just been created)
                     </p>
-                  </div>
+                    </div>
 
-                  <div className="typo-line">
-                    <p>
-                      <span className="category">Step 5:</span>
+                    <div className="typo-line">
+                      <p>
+                        <span className="category">Step 5:</span>
                       Create possible price points for your item for the model to explore.
                     </p>
-                  </div>
-                  <div className="typo-line">
-                    <p>
-                      <span className="category">Step 6:</span>
+                    </div>
+                    <div className="typo-line">
+                      <p>
+                        <span className="category">Step 6:</span>
                       Note down StoreID and ItemID of the item, they will be required in later steps.
                     </p>
-                  </div>
-                  <div className="typo-line">
-                    <p>
-                      <span className="category">Step 7:</span>
+                    </div>
+                    <div className="typo-line">
+                      <p>
+                        <span className="category">Step 7:</span>
                       In your Unity Project, create a new object. In this example, we will name it “DP Integrator”.
                     </p>
-                  </div>
-                  <div className="typo-line">
-                    <p>
-                      <span className="category">Step 8:</span>
+                    </div>
+                    <div className="typo-line">
+                      <p>
+                        <span className="category">Step 8:</span>
                       Add the following C# scripts to the object. The script’s name is irrelevant (in this example: DPAppIntegratorScript.cs)
                     </p>
-                  </div>
+                    </div>
+
+                  </Col>
+                  </Row>
+
 
                   <Row>
                     <Col lg={1}></Col>
                     <Col lg={10}>
-                      <CopyBlock
-                        text={step7code}
-                        language="csharp"
-                        showLineNumbers={true}
-                        theme={a11yLight}
-                        wrapLines
-                        codeBlock
-                      />
+                      <SyntaxHighlighter
+                        language="cs"
+                        style={okaidia}
+                        showLineNumbers
+                      >
+                        {step7code}
+                      </SyntaxHighlighter>
                     </Col>
                     <Col lg={1}></Col>
                   </Row>
 
-
-                  <div className="typo-line">
-                    <p>
-                      <span className="category">Step 9:</span>
+                  <Row>
+                    <Col lg={10}>
+                      <div className="typo-line">
+                        <p>
+                          <span className="category">Step 9:</span>
                       Everything is now ready. You can start using the functions available in the script. Here are brief explanations of what each function does to get you started:
                     </p>
-                  </div>
-                  <div>
-                    <Row>
-                      <Col lg={1}></Col>
-                      <Col lg={10}>
-                        <CopyBlock
-                          text={step9code}
-                          language="csharp"
-                          showLineNumbers={true}
-                          theme={a11yLight}
-                          wrapLines
-                          codeBlock
-                        />
-                      </Col>
-                      <Col lg={1}></Col>
-                    </Row>
-                  </div>
-                  <div className="typo-line">
-                    <p>
-                      <b>ModelUpdate</b> is a public function that can be called to send a new request to the application's backend. The function returns a new optimal price point, given the data sent with the request. <br />
-                      <b>Arguments:</b> <i>storeId, itemId,</i> the accumulated <i>demand</i> (purchases) are required. Optionally, a <i>callback</i> function can also be passed for price label updates, or other uses.
-                  </p>
-                  </div>
-                  <div className="typo-line">
-                    <p>
-                      <span className="category">Step 10:</span>
-                      An example of a an item in a store that updates its price every 10 seconds can be found here:
-                    </p>
-                  </div>
+                      </div>
+                    </Col>
+                  </Row>
 
                   <Row>
                     <Col lg={1}></Col>
                     <Col lg={10}>
-                      <CopyBlock
-                        text={step10code}
-                        language="csharp"
-                        showLineNumbers={true}
-                        theme={a11yLight}
-                        wrapLines
-                        codeBlock
-                      />
+                      <SyntaxHighlighter
+                        language="cs"
+                        style={okaidia}
+                        showLineNumbers
+                        startingLineNumber={10}>
+                        {step9code}
+                      </SyntaxHighlighter>
                     </Col>
                     <Col lg={1}></Col>
                   </Row>
 
+                  <Row>
+                    <Col lg={10}>
+                      <div className="typo-line">
+                        <p>
+                          <b>ModelUpdate</b> is a public function that can be called to send a new request to the application's backend. The function returns a new optimal price point, given the data sent with the request. <br />
+                          <b>Arguments:</b> <i>storeId, itemId,</i> the accumulated <i>demand</i> (purchases) are required. Optionally, a <i>callback</i> function can also be passed for price label updates, or other uses.
+                        </p>
+                      </div>
+                      <div className="typo-line">
+                        <p>
+                          <span className="category">Step 10:</span>
+                      An example of a an item in a store that updates its price every 10 seconds can be found here:
+                    </p>
+                      </div>
+                    </Col>
+                  </Row>
+
+                  <Row>
+                    <Col lg={1}></Col>
+                    <Col lg={10}>
+                      <SyntaxHighlighter language="cs" style={okaidia} showLineNumbers>
+                        {step10code}
+                      </SyntaxHighlighter>
+                    </Col>
+                    <Col lg={1}></Col>
+                  </Row>
                 </div>
               }
             />
